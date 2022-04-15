c891c2b13f65a5e4f3a7ea8868bf62e59c365afa
Manisha Agarwal <manisha.agarwal@classicinformatics.com>
2022-04-15 09:59:05 +0530

    Integrate GET API for initial count

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CounterValue from "./CounterValue";
 
 const Counter = () => {
   const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    getInitialCount();
+  }, []);
+
+  const getInitialCount = async () => {
+    try {
+      const res = await axios.get(
+        "https://interview-8e4c5-default-rtdb.firebaseio.com/front-end/8387039003.json"
+      );
+      if (res?.status === 200 && res?.data?.counter1) {
+        setCount(res.data.counter1);
+      }
+    } catch (err) {
+      console.log({ err });
+    }
+  };
 
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
